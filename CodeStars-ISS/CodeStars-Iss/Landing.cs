e2a2cd8a3bbcca74266e70b379d15c05fb2658dd
Raluca Manea <raluca96ok@gmail.com>
2017-05-20 15:05:15 +0300

    + pret bilet + full screen la inceput + datagridview

--- CodeStars-ISS/CodeStars-Iss/Landing.cs
+++ CodeStars-ISS/CodeStars-Iss/Landing.cs
@@ -14,10 +14,10 @@ namespace CodeStars_Iss
     //ButtonAddConference- adauga conferinta - la click deschide AddConference form
     //ButtonMyConferences - Apare optiunea de a adauga co-chairs pentru conferinta ta
     //ButtonAllConferences
-    //ConferencesList - lista conferinte
+    //GridViewConferinte - lista conferinte
     //ButtonBuyTicket - daca am selectat CheckBoxListener se cumpara automat bilet ( inainte selectam conferinta pt care dorim sa cumparam )
                       //-daca am selectat CheckBoxSpeaker - deschide BuyTicketSpeaker 
-    //ButtonFullScreen 
+ 
     //La apasarea ESC de anuleaza fullscreen mode
                     
     public partial class Landing : Form
@@ -28,27 +28,11 @@ namespace CodeStars_Iss
             ctrl = c;
             InitializeComponent();
             PanelCoChair.Visible = false;
+            this.WindowState = FormWindowState.Maximized;
         }
 
         private void Landing_Load(object sender, EventArgs e)
         {
-        }
-
-        private void FullScreenButton_Click(object sender, EventArgs e)
-        {
-            FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-            TopMost = true;
-        }
-
-        private void Landing_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Escape)
-            {
-                FormBorderStyle = FormBorderStyle.Sizable;
-                WindowState = FormWindowState.Normal;
-                TopMost = false;
-            }
         }
 
         private void ButtonMyConferences_Click(object sender, EventArgs e)
